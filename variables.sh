32b07220dd365c86cf18d2697d5cd34ed127ad34
Eenclaver <soshnevand@gmail.com>
2026-05-05 12:20:37 +0300

    BREAKING: added variable practive, too many "BREAKING" flags though...will fix probably fix itself when lessons will become more hard

---
+++ variables.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+#Well, they do kinda work the same in every language, but well, practice make perferct.
+
+num_one=1
+
+num_two=2
+
+echo -e "$num_one is smaller then $num_two. And also, \nhere is an example how to write a dollar without doing it like a variable - \$1 here is a dollar, yay"
+
+sum=$((5+5))
+
+echo $sum
+
+var1=$(( $num_two - $num_one))
+
+echo $var1
